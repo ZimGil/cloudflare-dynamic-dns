c6ab409e614ab897a9aa5700ee030b7e0b76be3f
ZimGil <gil.tichon@gmail.com>
2020-05-12 13:10:06 +0300

    cron schedule based on env var

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import publicIp from 'public-ip';
+import cron from 'node-cron';
 
 import { getRootARecordContent, saveIp } from './ip-manager';
 import logger from './logger';
 
-const ipPromises = [
-  publicIp.v4().then((ip) => (logger.debug(`Current IP: ${ip}`), ip)),
-  getRootARecordContent()
-];
+const interval = process.env.INTERVAL_IN_MINUTES;
 
-Promise.all(ipPromises)
-  .then(([currentIp, knownIp]) => currentIp !== knownIp ? saveIp(currentIp) : noop())
-  .catch(logger.error);
+if (!interval) {
+  run();
+} else {
+  let cronExpression = `*/${interval} * * * *`;
+  cronExpression = cron.validate(cronExpression) ? cronExpression : '0 * * * *'
+  cron.schedule(cronExpression, run);
+}
+
+
+function run() {
+  const ipPromises = [
+    publicIp.v4().then((ip) => (logger.debug(`Current IP: ${ip}`), ip)),
+    getRootARecordContent()
+  ];
+
+  Promise.all(ipPromises)
+    .then(([currentIp, knownIp]) => currentIp !== knownIp ? saveIp(currentIp) : noop())
+    .catch(logger.error);
+}
 
 function noop() {
   logger.info('Current record matches current IP')
