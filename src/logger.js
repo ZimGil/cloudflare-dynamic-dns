9f67d1367102014f2dff0d75c0a212351562bdc3
ZimGil <gil.tichon@gmail.com>
2020-05-16 16:50:00 +0300

    add separate error log + roll logs at 1MB

--- src/logger.js
+++ src/logger.js
@@ -3,11 +3,13 @@ import log4js from 'log4js';
 log4js.configure({
   appenders: {
     stdout: { type: 'stdout' },
-    file: { type: 'file', filename: 'debug.log' },
-    ipChange: {type: 'file', filename: 'ip-change.log'}
+    file: { type: 'file', filename: 'logs/debug.log', maxLogSize: 1048576, compress: true, keepFileExt: true },
+    ipChange: { type: 'file', filename: 'logs/ip-change.log'},
+    errorsFile: { type: 'file', filename: 'logs/error.log' },
+    errorsOnly: { type: 'logLevelFilter', appender: 'errorsFile', level: 'error' }
   },
   categories: {
-    default: { appenders: [ 'stdout', 'file' ], level: process.env.LOG_LEVEL || 'debug' },
+    default: { appenders: [ 'stdout', 'file', 'errorsOnly' ], level: process.env.LOG_LEVEL || 'debug' },
     ipChange: { appenders: [ 'stdout', 'ipChange' ], level: process.env.LOG_LEVEL || 'info' }
   }
 });
